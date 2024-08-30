69a8e6a1d5d4af864170bdcef3497a3e14305285
Unnati Gaglani <unnatigaglani93@gmail.com>
2024-08-30 14:42:45 -0700

    commit

--- Kal Assignments/tv-show-app/src/app/app.component.ts
+++ Kal Assignments/tv-show-app/src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {ResultComponent} from './result/result.component';
+
+
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ResultComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'tv-show-app';
 }
+
+
---
+++ Kal Assignments/tv-show-app/src/app/result/result.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-result',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './result.component.html',
+  styleUrl: './result.component.css'
+})
+export class ResultComponent {
+
+}
